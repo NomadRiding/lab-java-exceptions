6f3bbb054e0fbc6a8a466dd4cdd9232a1a013869
nomadriding <fundora.a@gmail.com>
2024-06-19 13:23:28 -0400

    fix PersonsList

--- Extension/src/main/java/org/example/PersonsList.java
+++ Extension/src/main/java/org/example/PersonsList.java
@@ -1,67 +1,48 @@
 package org.example;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.UUID;
 
-public class PersonsList {
-    private List<Person> persons;
+public class PersonList {
+    private List<Person> list = new ArrayList<>();
 
-    public PersonsList() {
-        this.persons = new ArrayList<>();
-    }
 
-    public void addPerson(Person person) {
-        persons.add(person);
-    }
+    //METHODS
+    public Person findByName(String n)  {
+        if (n.split(" ").length != 2)
+            throw new IllegalArgumentException("Invalid name format. It should be 'firstName lastName'.");
 
-    public boolean removePerson(Person person) {
-        return persons.remove(person);
-    }
-
-    public List<Person> getPersons() {
-        return new ArrayList<>(persons);
-    }
-
-    public Person findByName(String name) {
-        validateNameFormat(name);
-        for (Person person : persons) {
-            if (person.getName().equals(name)) {
-                return person;
+        for (Person p : list) {
+            if (p.getName().equals(n)) {
+                return p;
             }
         }
         return null;
     }
 
-    private void validateNameFormat(String name) {
-        String namePattern = "[A-Z][a-zA-Z]* [A-Z][a-zA-Z]*";
-        if (!Pattern.matches(namePattern, name)) {
-            throw new IllegalArgumentException("Name must be in the format 'FirstName LastName'");
-        }
+    public Person clone(Person p) {
+        return new Person(UUID.randomUUID().hashCode(), p.getName(), p.getAge(), p.getOccupation());
+
     }
 
-    public Person clone(Person person, int newId) {
-        return new Person(newId, person.getName(), person.getAge(), person.getOccupation());
+    public void writePersonToFile (Person p) {
+        try{
+            FileWriter fileWriter = new FileWriter("person.txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println(p.toString());
+            printWriter.close();
+        }catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
 
-    public void writePersonToFile(Person person, String filePath) {
-        BufferedWriter writer = null;
-        try {
-            writer = new BufferedWriter(new FileWriter(filePath));
-            writer.write(person.toString());
-        } catch (IOException e) {
-            System.err.println("Error writing to file: " + e.getMessage());
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    System.err.println("Error closing file writer: " + e.getMessage());
-                }
-            }
-        }
+    //ARRAYLIST METHODS
+    public List<Person> getList(){
+        return list;
     }
 }
