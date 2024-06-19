f46a98a0b15c8a4073dbb71d0d3e8e63d59298b2
nomadriding <fundora.a@gmail.com>
2024-06-19 13:29:44 -0400

    fix personText

--- Extension/src/test/java/PersonTest.java
+++ Extension/src/test/java/PersonTest.java
@@ -1,38 +1,14 @@
 package org.example;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PersonListTest {
+class PersonTest {
 
     @Test
-    void findByName1() {
-        PersonList personList = new PersonList();
-        Person p = new Person(1, "Jenna Doe", 30, "Software Developer");
-        personList.getList().add(p);
-        Person foundPerson = personList.findByName("Jenna Doe");
-        assertEquals(p, foundPerson);
-    }
-
-    @Test
-    void findByName2() {
-        PersonList personList = new PersonList();
-        Person p = new Person(1, "Jenna Doe", 30, "Software Developer");
-        personList.getList().add(p);
-        assertThrows(IllegalArgumentException.class, () -> personList.findByName("Jenna_Doe"));
-    }
-
-    @Test
-    void testClone() {
-        PersonList personList = new PersonList();
+    void setAge() {
         Person p = new Person(1, "John Doe", 30, "Software Developer");
-        Person clonedPerson = personList.clone(p);
-        assertEquals(p.getName(), clonedPerson.getName());
-        assertEquals(p.getAge(), clonedPerson.getAge());
-        assertEquals(p.getOccupation(), clonedPerson.getOccupation());
-        assertNotEquals(p.getId(), clonedPerson.getId());
-
+        assertThrows(IllegalArgumentException.class, () -> p.setAge(-1));
 
     }
 }
